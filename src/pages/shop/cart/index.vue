b50b610630afaff4bf1bd1cdce8f77139ad98543
indexpro24@gmail.com <indexpro24@gmail.com>
2021-04-11 22:32:46 +0300

    master

--- src/pages/shop/cart/index.vue
+++ src/pages/shop/cart/index.vue
@@ -2,7 +2,7 @@
   <div class="bg-gray-50">
     <div class="p-10 mx-auto sm:px-6 lg:px-8 bg-gradient-to-b from-blueGray-300 to-gray-50 shadow-lg mb-4">
       <div class="flex flex-col text-center w-full">
-        <h1 class=" text-5xl font-medium title-font text-gray-900">Корзина</h1>
+        <h1 class=" text-5xl font-medium title-font text-gray-900">Корзина (заказы по почте временно не принимаются, пожалуйста звоните)</h1>
       </div>
     </div>
 
