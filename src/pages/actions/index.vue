e4c62cd5771429438998634dd23231f8702abec0
indexpro24@gmail.com <indexpro24@gmail.com>
2021-10-27 22:31:31 +0300

    master

--- src/pages/actions/index.vue
+++ src/pages/actions/index.vue
@@ -7,6 +7,32 @@
       </div>
     </div>
 
+    <div class="mt-20 max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="text-center">
+        <p class="text-base leading-6 text-teal-800 font-semibold tracking-wide uppercase"></p>
+        <h2 class=" mt-2 text-6xl leading-12 font-extrabold tracking-tight text-gray-800 sm:text-5xl sm:leading-10">
+          Наши акции
+        </h2>
+        <p class="mt-4 max-w-7xl text-xl leading-7 text-gray-500 lg:mx-auto">
+          Для приобретения данной группы товаров по специальной цене свяжитесь с нашим менеджером по телефону
+        </p>
+        <div class="">
+          <!--          <svg aria-hidden="true" class="h-12 w-12" fill="none" stroke="currentColor"-->
+          <!--               viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">-->
+          <!--            <path-->
+          <!--              d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"-->
+          <!--              stroke-linecap="round" stroke-linejoin="round"-->
+          <!--              stroke-width="2"/>-->
+          <!--          </svg>-->
+          <p class="m-4 text-5xl font-bold text-red-900 animate-pulse">
+            8 (930) 688-88-62
+          </p>
+        </div>
+
+      </div>
+    </div>
+
+
     <div class="py-8 sm:py-12">
       <div class="relative sm:py-16">
         <div aria-hidden="true" class="hidden sm:block">
